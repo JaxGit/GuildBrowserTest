a77ecca3dd415680817aa5b051509a95bc018109
JaxGit <rainbow@rainbows-Mac.local>
2015-06-18 01:16:17 +1000

    add a test suite for Character, including a break test case using local json test data.

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by jax on 18/06/2015.
+//  Copyright (c) 2015 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
